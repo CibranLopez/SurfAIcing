a3c4f8a3af0b419ce00a071ed8ba041f360855c8
cibran <cibranlpz@gmail.com>
2025-03-07 17:50:53 +0100

    Print energies for checking

--- avg-pot.py
+++ avg-pot.py
@@ -13,7 +13,7 @@ vacuum_mac_pot = mac_pot[mid_idx:]
 
 # Bulk
 idx_ref = int(0.25*len(bulk_mac_pot))
-bulk_valid_mac_pot = mac_pot[idx_ref:-idx_ref]
+bulk_valid_mac_pot = bulk_mac_pot[idx_ref:-idx_ref]
 mean_avg_pot_bulk = np.mean(bulk_valid_mac_pot[: ,1])
 
 print(f'Mean vacuum: {mean_avg_pot_bulk}')
@@ -22,11 +22,11 @@ plt.plot([bulk_valid_mac_pot[0, 0], bulk_valid_mac_pot[-1, 0]], [mean_avg_pot_bu
 
 # Vacuum
 idx_ref = int(0.25*len(vacuum_mac_pot))
-vacuum_valid_mac_pot = mac_pot[idx_ref:-idx_ref]
+vacuum_valid_mac_pot = vacuum_mac_pot[idx_ref:-idx_ref]
 mean_avg_pot_vacuum = np.mean(vacuum_valid_mac_pot[: ,1])
 
 print(f'Mean vacuum: {mean_avg_pot_vacuum}')
 plt.plot(vacuum_valid_mac_pot[:, 0], vacuum_valid_mac_pot[:, 1], label='Vacuum')
-plt.plot([mean_avg_pot_vacuum[0, 0], mean_avg_pot_vacuum[-1, 0]], [mean_avg_pot_vacuum, mean_avg_pot_vacuum])
+plt.plot([vacuum_valid_mac_pot[0, 0], vacuum_valid_mac_pot[-1, 0]], [mean_avg_pot_vacuum, mean_avg_pot_vacuum])
 
 plt.show()
